8b45a0dba028733f491a40915cc1ed1eab4762ac
Raleigh Littles III <raleighlittles@gmail.com>
2023-05-11 12:50:15 -0700

    make sure every created filename has an extension

--- sms_extractor.py
+++ sms_extractor.py
@@ -19,20 +19,24 @@ def reconstruct_sms_images(sms_xml_dir, output_images_dir):
 
             # '*' technically isn't a valid MIME type but for some reason this application uses it
             # https://www.iana.org/assignments/media-types/media-types.xhtml#image
-            image_ext_types = ["gif", "png", "jpeg", "*"]
+            image_ext_types = ["avif", "bmp", "gif", "heic", "heif", "jpeg", "tiff", "png", "webp", "*"]
             xpath_search_str_base = ".//part[@ct='image/"
 
             for ext in image_ext_types:
+                # see https://www.synctech.com.au/sms-backup-restore/fields-in-xml-backup-files/ for a description of the XML fields
                 xpath_search_expr = xpath_search_str_base + ext + "']"
                 b64_results_list.append(
-                    [(b.attrib['data'], b.attrib['cl']) for b in root.findall(xpath_search_expr)])
+                    [(b.attrib['data'], b.attrib['cl'], ext) for b in root.findall(xpath_search_expr)])
 
             for result_type in b64_results_list:
-                for (data, cl) in result_type:
+                for (data, content_location, ext) in result_type:
                     # If it's empty, just assign it a random 10-letter string (so that it doesn't conflict with other unnamed files)
-                    if cl == "" or cl == "null":
-                        cl = "".join(random.sample(string.ascii_letters, 10))
-                    with open(os.path.join(output_images_dir, cl), 'wb') as f:
+                    if content_location == "" or content_location == "null":
+                        content_location = "".join(random.sample(string.ascii_letters, 10)) + "." + ext
+
+                    # this ensures the filename has an extension, if it doesn't (likely) have one
+                    output_filename = (content_location if '.' in content_location else (content_location + "." + ext))
+                    with open(os.path.join(output_images_dir, output_filename), 'wb') as f:
                         f.write(base64.b64decode(data))
                         orig_files_count += 1
 
